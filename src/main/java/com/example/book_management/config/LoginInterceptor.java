2b9288742a93d9916acea2a81010a3f6101741ad
cccuuuhhh <371315871@qq.com>
2025-01-28 11:12:20 +0800

    升级到jdk17

--- src/main/java/com/example/book_management/config/LoginInterceptor.java
+++ src/main/java/com/example/book_management/config/LoginInterceptor.java
@@ -1,8 +1,8 @@
 package com.example.book_management.config;
 
 import org.springframework.web.servlet.HandlerInterceptor;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 public class LoginInterceptor implements HandlerInterceptor {
 
